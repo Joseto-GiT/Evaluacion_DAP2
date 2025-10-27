8a4883b7c8dc2af437017916d78934344c7950c6
Joseto-GiT <jt.rojas@duocuc.cl>
2025-10-27 15:58:48 -0300

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.tiendaropa
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.tiendaropa.navigation.AppNavigation
+import com.example.tiendaropa.ui.theme.TiendaDeRopaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TiendaDeRopaTheme {
+                TiendaDeRopaApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun TiendaDeRopaApp() {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        AppNavigation()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TiendaDeRopaAppPreview() {
+    TiendaDeRopaTheme {
+        TiendaDeRopaApp()
+    }
+}
